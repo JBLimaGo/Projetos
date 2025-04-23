d997fa917e9d0b08ad1f462bc60206f85e56c532
Jefferson Lima <jbldesenvolvimento@gmail.com>
2025-04-23 14:38:49 -0300

    Criação e uso de Componente no HTML

---
+++ curso-angular-Cli17/projeto-modulos/src/app/curso/curso.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CursoComponent } from './curso.component';
+
+@NgModule({
+  declarations: [CursoComponent],
+  imports: [CommonModule],
+  exports: [CursoComponent],
+})
+export class CursoModule {}
