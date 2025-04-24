3c39796128b9c1b012c68d91ee0bc8554bf0bc25
Jefferson Lima <jbldesenvolvimento@gmail.com>
2025-04-24 15:28:43 -0300

    Event Binding

--- curso-angular-Cli17/projeto-modulos/src/app/data-binding/data-binding.component.ts
+++ curso-angular-Cli17/projeto-modulos/src/app/data-binding/data-binding.component.ts
@@ -3,23 +3,34 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-data-binding',
   templateUrl: './data-binding.component.html',
-  styleUrl: './data-binding.component.css'
+  styleUrl: './data-binding.component.css',
 })
 export class DataBindingComponent implements OnInit {
- 
-  url: string = 'http://gmail.com';  
+  url: string = 'http://gmail.com';
 
-  urlImagem = 'https://fastly.picsum.photos/id/296/200/300.jpg?hmac=3w6L7NcSbkDRHC36vvfj4JuF0yOHmTjqQS5F9biJyKA'
+  valorAtual: string = '';
+  valorSalvo: string = '';
 
-  getValor(){
+  urlImagem =
+    'https://fastly.picsum.photos/id/296/200/300.jpg?hmac=3w6L7NcSbkDRHC36vvfj4JuF0yOHmTjqQS5F9biJyKA';
+
+  getValor() {
     return 1;
-  }     
-
-  constructor() { }
-
-
-  ngOnInit(){
-    
   }
 
+  botaoClicado() {
+    alert('Botão clicado!');
+  }
+
+  onkeyUp(event: KeyboardEvent) {
+    this.valorAtual = (<HTMLInputElement>event.target).value;
+  }
+
+  constructor() {}
+
+  salvarValor(valor: string) {
+    this.valorSalvo = valor; // Atualiza o valor atual com o valor recebido
+  }
+
+  ngOnInit() {}
 }
