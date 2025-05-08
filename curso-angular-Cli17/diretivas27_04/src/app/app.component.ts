43379ef8c7c4b32c84cefb681ccf57f742e5fab2
JBLima <jefferson.lima.pb@gmail.com>
2025-05-08 19:43:11 -0300

    Criação de Painel do BootStrap

--- curso-angular-Cli17/diretivas27_04/src/app/app.component.ts
+++ curso-angular-Cli17/diretivas27_04/src/app/app.component.ts
@@ -2,11 +2,12 @@ import { DiretivaNgswitchComponent } from './diretiva-ngswitch/diretiva-ngswitch
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { DiretivaNgifComponent } from './diretiva-ngif/diretiva-ngif.component';
+import { ExemploNgContentComponent } from './exemplo-ng-content/exemplo-ng-content.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, DiretivaNgifComponent, DiretivaNgswitchComponent],
+  imports: [RouterOutlet, DiretivaNgifComponent, DiretivaNgswitchComponent, ExemploNgContentComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
