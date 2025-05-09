13969c2c9e99fb95ea4df0d7682c23ae7b279ab9
JBLima <jefferson.lima.pb@gmail.com>
2025-05-08 21:50:09 -0300

    Landing Page

--- notus/public/assets/js/scripts.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const btn = document.querySelector('.btn');
-    if (btn) {
-        btn.addEventListener('click', () => {
-            alert('Entre em contato conosco para mais informações.');
-        
-        });
-    }
-});
