c6adfa4e434ee3c5b38e84a21e07720c9353f8ab
JBLima <jefferson.lima.pb@gmail.com>
2025-05-10 19:33:05 -0300

    Usando Services em outras classes

---
+++ curso-angular-Cli17/servicos/src/app/criar-curso/criar-curso.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+
+import { CursosService } from '../cursos/cursos.service';
+
+@Component({
+  selector: 'app-criar-curso',
+  standalone: false,
+  templateUrl: './criar-curso.component.html',
+  styleUrl: './criar-curso.component.css'
+})
+export class CriarCursoComponent {
+
+  cursos: string[] = [];
+
+  constructor(private cursosService: CursosService) {
+    //this.cursosService = new CursosService(); 
+    
+  }
+
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    this.cursos = this.cursosService.getCursos();
+  }
+
+}
--- curso-angular-Cli17/servicos/src/app/cursos/cursos.service.ts
+++ curso-angular-Cli17/servicos/src/app/cursos/cursos.service.ts
@@ -1,3 +1,6 @@
+import { Injectable } from '@angular/core';
+
+@Injectable()
 export class CursosService {
   private cursos: string[] = ['Angular 2', 'JavaScript', 'Java'];
 
