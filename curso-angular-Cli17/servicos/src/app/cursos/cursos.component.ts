86b2389ef2b45997e65b0ff4f79ce6af1dcbe874
JBLima <jefferson.lima.pb@gmail.com>
2025-05-10 16:58:16 -0300

    Serviços - Classes que comunica com o banco de dados

---
+++ curso-angular-Cli17/servicos/src/app/cursos/cursos.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+
+import { CursosService } from './cursos.service';
+
+@Component({
+  selector: 'app-cursos',
+  standalone: false,
+  templateUrl: './cursos.component.html',
+  styleUrl: './cursos.component.css'
+})
+export class CursosComponent {
+
+  cursos: string[] = [];
+  cursosService: CursosService;
+
+  constructor() {
+    this.cursosService = new CursosService();    
+  }
+
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    this.cursos = this.cursosService.getCursos();
+  }
+
+}
---
+++ curso-angular-Cli17/servicos/src/app/cursos/cursos.service.ts
@@ -0,0 +1,19 @@
+export class CursosService {
+  private cursos: string[] = ['Angular 2', 'JavaScript', 'Java'];
+
+  constructor() {
+    console.log('CursosService');
+  }
+
+  getCursos(): string[] {
+    return ['Angular 2', 'JavaScript', 'Java'];//this.cursos;
+  }
+
+  addCurso(curso: string): void {
+    this.cursos.push(curso);
+  }
+
+  getCurso(index: number): string {
+    return this.cursos[index];
+  }
+}
