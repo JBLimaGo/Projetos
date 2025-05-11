f68647af8fb52edc47e044ea5bab46e3a4baf102
JBLima <jefferson.lima.pb@gmail.com>
2025-05-11 14:09:28 -0300

    Aplicando Locale (internacionalização) nos Pipes

--- curso-angular-Cli17/pipes/src/app/app.module.ts
+++ curso-angular-Cli17/pipes/src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -6,18 +6,26 @@ import { AppComponent } from './app.component';
 import { ExemplosPipesComponent } from './exemplos-pipes/exemplos-pipes.component';
 import { CamelCasePipe } from './camel-case.pipe';
 
+// Importa e registra o locale pt-BR
+import { registerLocaleData } from '@angular/common';
+import localePt from '@angular/common/locales/pt';
+registerLocaleData(localePt);
+
 @NgModule({
   declarations: [
     AppComponent,
     ExemplosPipesComponent,
-    CamelCasePipe
+    CamelCasePipe 
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    
+    AppRoutingModule,    
   ],
-  providers: [],
+  providers: [
+    {
+      provide: LOCALE_ID,  useValue: 'pt-BR'  // Define o locale padrão como pt-BR
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
