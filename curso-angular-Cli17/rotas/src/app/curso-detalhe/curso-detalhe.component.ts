48858e0d9c6052bfbc56116f3089b7c0fb375593
JBLima <jefferson.lima.pb@gmail.com>
2025-05-13 19:27:15 -0300

    Rotas: Escutando mudanças nos parâmetros de roteamento

--- curso-angular-Cli17/rotas/src/app/curso-detalhe/curso-detalhe.component.ts
+++ curso-angular-Cli17/rotas/src/app/curso-detalhe/curso-detalhe.component.ts
@@ -1,19 +1,34 @@
-import { Value } from './../../../../pipes/node_modules/regjsparser/parser.d';
-import { Component } from '@angular/core';
+
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+
 
 @Component({
   selector: 'app-curso-detalhe',
   templateUrl: './curso-detalhe.component.html',
-  styleUrl: './curso-detalhe.component.css'
+  styleUrls: ['./curso-detalhe.component.css']
 })
-export class CursoDetalheComponent {
+export class CursoDetalheComponent implements OnInit {
 
 
   id: string = '';
+  inscricao: Subscription = new Subscription();
 
   constructor(private route: ActivatedRoute, private router: Router) {
     this.id = this.route.snapshot.params['id'];
+  }
+
+  ngOnInit() {
+    this.route.params.subscribe((params) => {
+      this.id = params['id'];
+    });
+  }
+
+  ngOnDestroy(): void {
+    //Called once, before the instance is destroyed.
+    //Add 'implements OnDestroy' to the class.
+    this.inscricao.unsubscribe();
   }
 
 }
