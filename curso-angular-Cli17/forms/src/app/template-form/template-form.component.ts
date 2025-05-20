ff4f15ff886ebeca4befefd803966a46e943f6b2
Jefferson Lima <jbldesenvolvimento@gmail.com>
2025-05-20 13:24:15 -0300

    Forms (template driven) Controles ngForm, ngSubmit e ngModel

--- curso-angular-Cli17/forms/src/app/template-form/template-form.component.ts
+++ curso-angular-Cli17/forms/src/app/template-form/template-form.component.ts
@@ -3,8 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-template-form',
   templateUrl: './template-form.component.html',
-  styleUrl: './template-form.component.css'
+  styleUrl: './template-form.component.css',
 })
 export class TemplateFormComponent {
+  constructor() {}
 
+  onSubmit(form: any) {
+    console.log(form);
+  }
+
+  ngOnInit(): void {}
 }
