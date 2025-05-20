0d1cb8b6f2761ef6abfca10138b98cb204f6734b
Jefferson Lima <jbldesenvolvimento@gmail.com>
2025-05-20 15:07:02 -0300

    Forms (template driven) Módulos e FormsModule Forms (template driven) Aplicando validação nos campos

--- curso-angular-Cli17/forms/src/app/app.module.ts
+++ curso-angular-Cli17/forms/src/app/app.module.ts
@@ -4,21 +4,18 @@ import { FormsModule } from '@angular/forms'; // Import FormsModule for template
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { TemplateFormComponent } from './template-form/template-form.component';
+//import { TemplateFormComponent } from './template-form/template-form.component';
 import { DataFormComponent } from './data-form/data-form.component';
+import { TemplateFormModule } from './template-form/template-form.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TemplateFormComponent,
-    DataFormComponent
+    //TemplateFormComponent,
+    DataFormComponent,
   ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, FormsModule, AppRoutingModule, TemplateFormModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
